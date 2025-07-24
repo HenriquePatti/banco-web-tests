4d913d90bb80941557e73017cc09283311789486
Henrique.Patti <rezendepatti@gmail.com>
2025-07-24 16:01:46 -0300

    adiciona teste de login inválido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -20,4 +20,33 @@ describe('Login', () => {
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
 
   })
+
+  it('Deve exibir mensagem de erro quando informado credenciais inválidas', () => {
+    // Arrange
+    cy.visit('http://localhost:4000')
+
+    // Act
+    cy.get('#username')
+      .click()
+      .type('julio.lima')
+
+    cy.get('#senha')
+      .click()
+      .type('12567')
+
+    cy.clock()
+    cy.get('button')
+      .contains('Entrar')
+      .click()  
+
+    // Assert
+    cy.get('#toast-container')
+      .should('have.text', 'Erro no login. Tente novamente.')
+      .and('be.visible')
+
+    cy.tick(5000)
+    cy.get('#toast-container')
+      .should('not.exist')
+  
+  })
 })
