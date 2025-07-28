c527251aae753b44e8f07bfbc54c7e60ba875525
Henrique.Patti <rezendepatti@gmail.com>
2025-07-28 20:48:53 -0300

    altera testes de login para uso de variáveis de ambiente

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,55 +1,57 @@
-describe('Login', () => {
-  
-  beforeEach( ()=> {
+describe("Login", () => {
+  beforeEach(() => {
     // Arrange
-    cy.visit('http://localhost:4000')
-  })
+    cy.visit('/');
+  });
 
-  it('Deve acessar o sistemas com credenciais validas', () => {
+  it("Deve acessar o sistemas com credenciais validas", () => {
     // Act
-    cy.fixture("credenciais").then( (credenciais)=> {
-      cy.get('#username')
+    const username = Cypress.env('USERNAME');
+    const senha = Cypress.env('PASSWORD');
+    
+      cy.get("#username")
         .click()
-        .type(credenciais.valida.username, { log: false})
+        .type(username, { log: false });
 
-      cy.get('#senha')
-        .click()
-        .type(credenciais.valida.senha, { log: false})
-
-      cy.get('button')
-        .contains('Entrar')
-        .click() 
-      
-      // Assert
-      cy.contains('h4', 'Realizar Transferência').should('be.visible')
-    })
-  })
-
-  it('Deve exibir mensagem de erro quando informado credenciais inválidas', () => {
-    // Act
-    cy.fixture('credenciais').then( (credenciais) =>{
-      cy.get('#username')
+      cy.get("#senha")
       .click()
-      .type(credenciais.invalida.username)
+      .type(senha, { log: false });
 
-      cy.get('#senha')
-        .click()
-        .type(credenciais.invalida.senha)
-
-      cy.clock()
-        cy.get('button')
-        .contains('Entrar')
-        .click()  
+      cy.get("button")
+        .contains("Entrar")
+        .click();
 
       // Assert
-        cy.get('#toast-container')
-          .should('have.text', 'Erro no login. Tente novamente.')
-          .and('be.visible')
+      cy.contains("h4", "Realizar Transferência").should("be.visible");
+  
+  });
 
-      cy.tick(5000)
-        cy.get('#toast-container')
-        .should('not.exist')
-    })
-  })
+  it("Deve exibir mensagem de erro quando informado credenciais inválidas", () => {
+    // Act
+    const username = Cypress.env('USERNAME');
+    const senha = Math.random().toString(10).substring(2, 10)
+    
+      cy.get("#username")
+        .click()
+        .type(username);
 
-})
+      cy.get("#senha")
+        .click()
+        .type(senha);
+
+      cy.clock();
+        cy.get("button")
+          .contains("Entrar")
+          .click();
+
+      // Assert
+      cy.get("#toast-container")
+        .should("have.text", "Erro no login. Tente novamente.")
+        .and("be.visible");
+
+      cy.tick(5000);
+        cy.get("#toast-container")
+        .should("not.exist");
+  
+  });
+});
