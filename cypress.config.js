f6acd686543f8a00b03c7581a7dfd7f72e92313c
Henrique.Patti <rezendepatti@gmail.com>
2025-07-28 20:47:37 -0300

    adiciona variaveis de ambiente para receber dados sensíveis

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:4000',
+    env: {
+      // Segurança: outras credenciais vêm do process.env, nunca hardcoded
+      username: process.env.CYPRESS_USERNAME,
+      password: process.env.CYPRESS_PASSWORD
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
